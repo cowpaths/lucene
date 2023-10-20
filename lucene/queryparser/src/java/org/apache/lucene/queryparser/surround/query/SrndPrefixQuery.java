522f2b0df72f0feccc56e7b74d50170d37002f65
Michael Gibney <michael@michaelgibney.net>
2023-10-20 12:55:22 -0400

    quick-and-dirty enable automaton for srndquery prefix query

--- lucene/queryparser/src/java/org/apache/lucene/queryparser/surround/query/SrndPrefixQuery.java
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/surround/query/SrndPrefixQuery.java
@@ -22,17 +22,28 @@ import org.apache.lucene.index.MultiTerms;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.search.PrefixQuery;
 import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.StringHelper;
+import org.apache.lucene.util.automaton.CompiledAutomaton;
+import org.apache.lucene.util.automaton.Operations;
 
 /** Query that matches String prefixes */
 public class SrndPrefixQuery extends SimpleTerm {
   private final BytesRef prefixRef;
+  private final CompiledAutomaton compiled;
 
   public SrndPrefixQuery(String prefix, boolean quoted, char truncator) {
     super(quoted);
     this.prefix = prefix;
     prefixRef = new BytesRef(prefix);
+    PrefixQuery q = new PrefixQuery(new Term("dummy", prefixRef));
+    compiled =
+        new CompiledAutomaton(
+            q.getAutomaton(),
+            null,
+            true,
+            Operations.DEFAULT_DETERMINIZE_WORK_LIMIT,
+            q.isAutomatonBinary());
     this.truncator = truncator;
   }
 
@@ -46,10 +57,6 @@ public class SrndPrefixQuery extends SimpleTerm {
 
   public char getSuffixOperator() {
     return truncator;
-  }
-
-  public Term getLucenePrefixTerm(String fieldName) {
-    return new Term(fieldName, getPrefix());
   }
 
   @Override
@@ -68,32 +75,11 @@ public class SrndPrefixQuery extends SimpleTerm {
     /* inspired by PrefixQuery.rewrite(): */
     Terms terms = MultiTerms.getTerms(reader, fieldName);
     if (terms != null) {
-      TermsEnum termsEnum = terms.iterator();
+      TermsEnum termsEnum = compiled.getTermsEnum(terms);
 
-      boolean skip = false;
-      TermsEnum.SeekStatus status = termsEnum.seekCeil(new BytesRef(getPrefix()));
-      if (status == TermsEnum.SeekStatus.FOUND) {
-        mtv.visitMatchingTerm(getLucenePrefixTerm(fieldName));
-      } else if (status == TermsEnum.SeekStatus.NOT_FOUND) {
-        if (StringHelper.startsWith(termsEnum.term(), prefixRef)) {
-          mtv.visitMatchingTerm(new Term(fieldName, termsEnum.term().utf8ToString()));
-        } else {
-          skip = true;
-        }
-      } else {
-        // EOF
-        skip = true;
-      }
-
-      if (!skip) {
-        while (true) {
-          BytesRef text = termsEnum.next();
-          if (text != null && StringHelper.startsWith(text, prefixRef)) {
-            mtv.visitMatchingTerm(new Term(fieldName, text.utf8ToString()));
-          } else {
-            break;
-          }
-        }
+      BytesRef br;
+      while ((br = termsEnum.next()) != null) {
+        mtv.visitMatchingTerm(new Term(fieldName, BytesRef.deepCopyOf(br)));
       }
     }
   }
