0ed71deef155bdee6aeea794b38ff4e89d912ffc
Michael Gibney <michael@michaelgibney.net>
2024-10-07 16:43:10 -0400

    revert changes introduced w/ e96815ed51; will pull in from upstream

--- lucene/queryparser/src/java/org/apache/lucene/queryparser/surround/query/SrndTruncQuery.java
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/surround/query/SrndTruncQuery.java
@@ -17,34 +17,33 @@
 package org.apache.lucene.queryparser.surround.query;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.MultiTerms;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
-import org.apache.lucene.search.WildcardQuery;
 import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.automaton.CompiledAutomaton;
-import org.apache.lucene.util.automaton.Operations;
+import org.apache.lucene.util.StringHelper;
 
 /** Query that matches wildcards */
 public class SrndTruncQuery extends SimpleTerm {
-  public SrndTruncQuery(String truncated) {
+  public SrndTruncQuery(String truncated, char unlimited, char mask) {
     super(false); /* not quoted */
     this.truncated = truncated;
-    WildcardQuery wc =
-        new WildcardQuery(new Term("dummy", truncated), Operations.DEFAULT_DETERMINIZE_WORK_LIMIT);
-    compiled =
-        new CompiledAutomaton(
-            wc.getAutomaton(),
-            null,
-            true,
-            Operations.DEFAULT_DETERMINIZE_WORK_LIMIT,
-            wc.isAutomatonBinary());
+    this.unlimited = unlimited;
+    this.mask = mask;
+    truncatedToPrefixAndPattern();
   }
 
   private final String truncated;
-  private final CompiledAutomaton compiled;
+  private final char unlimited;
+  private final char mask;
+
+  private String prefix;
+  private BytesRef prefixRef;
+  private Pattern pattern;
 
   public String getTruncated() {
     return truncated;
@@ -55,16 +54,66 @@ public class SrndTruncQuery extends SimpleTerm {
     return getTruncated();
   }
 
+  protected boolean matchingChar(char c) {
+    return (c != unlimited) && (c != mask);
+  }
+
+  protected void appendRegExpForChar(char c, StringBuilder re) {
+    if (c == unlimited) re.append(".*");
+    else if (c == mask) re.append(".");
+    else re.append(c);
+  }
+
+  protected void truncatedToPrefixAndPattern() {
+    int i = 0;
+    while ((i < truncated.length()) && matchingChar(truncated.charAt(i))) {
+      i++;
+    }
+    prefix = truncated.substring(0, i);
+    prefixRef = new BytesRef(prefix);
+
+    StringBuilder re = new StringBuilder();
+    while (i < truncated.length()) {
+      appendRegExpForChar(truncated.charAt(i), re);
+      i++;
+    }
+    pattern = Pattern.compile(re.toString());
+  }
+
   @Override
   public void visitMatchingTerms(IndexReader reader, String fieldName, MatchingTermVisitor mtv)
       throws IOException {
+    int prefixLength = prefix.length();
     Terms terms = MultiTerms.getTerms(reader, fieldName);
     if (terms != null) {
-      TermsEnum termsEnum = compiled.getTermsEnum(terms);
+      Matcher matcher = pattern.matcher("");
+      try {
+        TermsEnum termsEnum = terms.iterator();
 
-      BytesRef br;
-      while ((br = termsEnum.next()) != null) {
-        mtv.visitMatchingTerm(new Term(fieldName, BytesRef.deepCopyOf(br)));
+        TermsEnum.SeekStatus status = termsEnum.seekCeil(prefixRef);
+        BytesRef text;
+        if (status == TermsEnum.SeekStatus.FOUND) {
+          text = prefixRef;
+        } else if (status == TermsEnum.SeekStatus.NOT_FOUND) {
+          text = termsEnum.term();
+        } else {
+          text = null;
+        }
+
+        while (text != null) {
+          if (StringHelper.startsWith(text, prefixRef)) {
+            String textString = text.utf8ToString();
+            matcher.reset(textString.substring(prefixLength));
+            if (matcher.matches()) {
+              mtv.visitMatchingTerm(new Term(fieldName, textString));
+            }
+          } else {
+            break;
+          }
+          text = termsEnum.next();
+        }
+      } finally {
+        matcher.reset();
       }
     }
   }
